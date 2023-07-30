204edcc1910d4dea8e276cc594e106c6bf7ac7e2
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2023-07-30 16:59:17 +0700

    chore(): update ssh & git config

--- home/ssh.nix
+++ home/ssh.nix
@@ -8,14 +8,14 @@ in
     enable = true;
     matchBlocks = {
       # Personal
-      personal = {
+      "github.com" = {
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_ed25519_personal";
       };
 
       # Paper.id
-      "github.com:paper-indonesia" = {
+      "github.com-paper" = {
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_ed25519_work";
