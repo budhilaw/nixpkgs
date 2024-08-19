d5ce245e3188ddccd10c204863f5fde872128057
Ericsson Budhilaw <ericsson.budhilaw@gmail.com>
2024-08-19 12:18:51 +0700

    feat(): add devShells

---
+++ nix/devShells.nix
@@ -0,0 +1,153 @@
+##################################################################
+#                       Development shells
+##################################################################
+{ self, ... }:
+
+{
+  perSystem =
+    { pkgs, config, ... }:
+    {
+      pre-commit.check.enable = true;
+      pre-commit.devShell = self.devShells.default;
+      pre-commit.settings.hooks = {
+        actionlint.enable = true;
+        shellcheck.enable = true;
+        nixfmt.enable = true;
+      };
+
+      devShells =
+        let
+          inherit (pkgs) lib;
+          mutFirstChar =
+            f: s:
+            let
+              firstChar = f (lib.substring 0 1 s);
+              rest = lib.substring 1 (-1) s;
+
+            in
+            # matched = builtins.match "(.)(.*)" s;
+            # firstChar = f (lib.elemAt matched 0);
+            # rest = lib.elemAt matched 1;
+            firstChar + rest;
+
+          toCamelCase_ =
+            sep: s:
+            mutFirstChar lib.toLower (lib.concatMapStrings (mutFirstChar lib.toUpper) (lib.splitString sep s));
+
+          toCamelCase =
+            s:
+            builtins.foldl' (s: sep: toCamelCase_ sep s) s [
+              "-"
+              "_"
+              "."
+            ];
+
+          mkNodeShell =
+            name:
+            let
+              node = pkgs.${name};
+              corepackShim = pkgs.nodeCorepackShims.overrideAttrs (_: {
+                buildInputs = [ node ];
+              });
+            in
+            pkgs.mkShell {
+              description = "${name} Development Environment";
+              buildInputs = [
+                node
+                corepackShim
+              ];
+            };
+
+          mkGoShell =
+            name:
+            let
+              go = pkgs.${name};
+            in
+            pkgs.mkShell {
+              description = "${name} Development Environment";
+              buildInputs = [ go ];
+              shellHook = ''
+                export GOPATH="$(${go}/bin/go env GOPATH)"
+                export PATH="$PATH:$GOPATH/bin"
+              '';
+            };
+
+          mkShell =
+            pkgName: name:
+            if lib.strings.hasPrefix "nodejs_" pkgName then
+              mkNodeShell name
+            else if lib.strings.hasPrefix "go_" pkgName then
+              mkGoShell name
+            else
+              builtins.throw "Unknown package ${pkgName} for making shell environment";
+
+          mkShells =
+            pkgName:
+            let
+              mkShell_ = mkShell pkgName;
+            in
+            builtins.foldl' (acc: name: acc // { "${toCamelCase name}" = mkShell_ name; }) { } (
+              builtins.filter (lib.strings.hasPrefix pkgName) (builtins.attrNames pkgs)
+            );
+
+        in
+        ####################################################################################################
+        #    see nodejs_* definitions in {https://search.nixos.org/packages?query=nodejs_}
+        #
+        #    versions: 14, 18, 20, 22, Latest
+        #
+        #    $ nix develop github:budhilaw/nixpkgs#<nodejsVERSION>
+        #
+        #
+        mkShells "nodejs_"
+        // mkShells "go_"
+        // {
+          default = pkgs.mkShell {
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          };
+
+          #
+          #
+          #    $ nix develop github:budhilaw/nixpkgs#go
+          #
+          #
+          go = pkgs.mkShell {
+            description = "Go Development Environment";
+            nativeBuildInputs = [ pkgs.go ];
+            shellHook = ''
+              export GOPATH="$(${pkgs.go}/bin/go env GOPATH)"
+              export PATH="$PATH:$GOPATH/bin"
+            '';
+          };
+
+          #
+          #
+          #    $ nix develop github:budhilaw/nixpkgs#rust-wasm
+          #
+          #
+          rust-wasm = pkgs.mkShell {
+            description = "Rust  Development Environment";
+            # declared ENV variables when starting shell
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+            nativeBuildInputs = with pkgs; [
+              rustc
+              cargo
+              gcc
+              rustfmt
+              clippy
+            ];
+          };
+
+          #
+          #
+          #    $ nix develop github:budhilaw/nixpkgs#bun
+          #
+          #
+          bun = pkgs.mkShell { buildInputs = [ pkgs.bun ]; };
+        };
+
+    };
+}
